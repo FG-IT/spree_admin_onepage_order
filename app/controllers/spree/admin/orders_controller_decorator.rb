bbcefb03a11d3694782683cd68e0a5604e23b7ea
JX <johnnyxiang2017@gmail.com>
2021-01-30 11:49:33 -0800

    resend shipment email

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -3,8 +3,24 @@ module Spree
     module OrdersControllerDecorator
       def show
         load_order
-
         @order.refresh_shipment_rates(ShippingMethod::DISPLAY_ON_BACK_END) unless @order.completed?
+      end
+
+      def resend
+        OrderMailer.confirm_email(@order.id, true).deliver_later
+        flash[:success] = Spree.t(:order_email_resent)
+
+        redirect_back fallback_location: spree.admin_order_url(@order)
+      end
+
+      def resend_shipped
+        load_order
+        @order.shipments.each do |shipment|
+          ShipmentMailer.shipped_email(shipment.id).deliver_later
+        end
+
+        flash[:success] = Spree.t(:order_shipment_email_resent)
+        redirect_back fallback_location: spree.admin_order_url(@order)
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Spree::Core::Engine.add_routes do
   # Add your extension routes here
   Spree::Core::Engine.add_routes do
     namespace :admin, path: Spree.admin_path do
-      resources :orders
+      resources :orders do
+        member do
+          post :resend_shipped
+        end
+      end
     end
   end
 end
